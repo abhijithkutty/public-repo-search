78a587ba37824712772b94031b1a8f8c6e6b64db
abhijithkutty <abhijithkutty8@gmail.com>
2022-06-08 14:25:07 +0530

    1st commit

---
+++ bb-search.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "\nEnter a valid  bitbucket username :"
+read user
+mkdir log/
+mkdir log/$user
+touch log/$user/repo.txt
+curl https://api.bitbucket.org/2.0/repositories/$user?fields=next,values.slug > log/$user/repo.txt
+var=$(grep -i "No workspace with identifier" log/$user/repo.txt | wc -l)
+if(($var == 0));
+then
+echo "\n"
+sed -e 's/slug//g' log/$user/repo.txt | tr -d '{}[]":'| tr ',' '\n'| sed -e 's/values/ /g' > log/$user/test.txt
+n=$(cat log/$user/test.txt |head| wc -l)
+echo "There are$n repositories for $user:"
+cat log/$user/test.txt
+else
+echo "\nInvalid user"
+fi
+rm -rf log/
